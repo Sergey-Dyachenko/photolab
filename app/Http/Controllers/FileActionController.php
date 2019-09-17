93a0d5cbd51129ab21420f82ba5cc7ed88b64cef
sergey <serjdyachenko@gmail.com>
2019-09-17 06:37:42 +0300

    first commit

---
+++ app/Http/Controllers/FileActionController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 17.09.2019
+ * Time: 6:02
+ */
+
+namespace App\Http\Controllers;
+use App\Http\Controllers;
+
+
+class FileActionController
+{
+    public function download
+}
